eb920d4764315f256bc1337702e375604998e2f9
Asgaard Developments <146543978+s4t4n667@users.noreply.github.com>
2026-02-26 14:23:23 +0000

    Bump version from 1.1.2 to 1.2.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 name 'lation_pawnshop'
 author 'iamlation'
-version '1.1.2'
+version '1.2.0'
 description 'A Pawn Shop script for ESX, QBCore & QBox using ox_inventory'
 
 server_scripts {
